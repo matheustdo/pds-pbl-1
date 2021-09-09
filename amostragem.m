b6ba8cbb9ab6603efaceacb9d0083c4726bd4bdb
Aloisio Junior <kleyner2@hotmail.com>
2021-09-09 11:33:39 -0300

    Update amostragem.m

--- amostragem.m
+++ amostragem.m
@@ -1,5 +1,5 @@
 clear; clc;
-[xt,fc,phi,t]=sinal(2,1,5000);
+[xt,fc,phi,t]=sinal(2,20,1000);
 filtro = designfilt('lowpassiir', 'PassbandFrequency', 1000, 'StopbandFrequency', 1200, 'PassbandRipple', 1, 'StopbandAttenuation', 60, 'SampleRate', 8000);
 sinal_filtrado = filter(filtro, xt);
 
@@ -46,3 +46,50 @@ plot(w,abs(2*X/length(t)));
 xlabel('$f$(Hz)','interpreter','latex');
 ylabel('Magnitude');
 title('Espectro $|X_{c}(j2\pi f)|$ Sinal Amostrado','interpreter','latex');
+
+% [yC,tC] = resample(t,s_amostrado,Fs,1,1,'spline');
+[yC,tC] = resample(s_amostrado,t,4000);
+figure(6)
+plot(tC,yC);
+grid on
+title('Resultado da Conversão D/C');
+
+figure(7);
+y4=fft(yC); grid on;
+yaux=fliplr(y4(1,2:end));
+X=[yaux y4];
+X(1,1:length(X)/4)=0;
+X(1,3*length(X)/4:end)=0;
+length(X);
+omega=0:Fs/length(y4):Fs-(Fs/length(y4));
+waux=-fliplr(omega(1,2:end));
+w=[waux omega];
+length(w);
+plot(w,abs(2*X/length(tC)));
+xlabel('$f$(Hz)','interpreter','latex');
+ylabel('Magnitude');
+title('Espectro $|X_{c}(j2\pi f)|$ Sinal Continuo','interpreter','latex');
+
+% filtroC = designfilt('lowpassiir', 'PassbandFrequency', 2000, 'StopbandFrequency', 2200, 'PassbandRipple', 1, 'StopbandAttenuation', 60, 'SampleRate', 8000);
+% sinal_filtradoC = filter(filtroC, yC);
+% 
+% figure(8)
+% plot(tC,sinal_filtradoC);
+% grid on
+% title('Resultado da Conversão D/C Filtrado');
+% 
+% figure(9);
+% y4=fft(sinal_filtradoC); grid on;
+% yaux=fliplr(y4(1,2:end));
+% X=[yaux y4];
+% X(1,1:length(X)/4)=0;
+% X(1,3*length(X)/4:end)=0;
+% length(X);
+% omega=0:Fs/length(y4):Fs-(Fs/length(y4));
+% waux=-fliplr(omega(1,2:end));
+% w=[waux omega];
+% length(w);
+% plot(w,abs(2*X/length(tC)));
+% xlabel('$f$(Hz)','interpreter','latex');
+% ylabel('Magnitude');
+% title('Espectro $|X_{c}(j2\pi f)|$ Sinal Continuo Filtrado','interpreter','latex');
