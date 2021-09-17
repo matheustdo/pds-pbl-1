7222b0122089b88ef1d1d606d243c3f4232d1e4f
AllanPS98 <30640347+AllanPS98@users.noreply.github.com>
2021-09-17 11:58:26 -0300

    atualizando plots

--- Refatoração/amostragem.m
+++ Refatoração/amostragem.m
@@ -6,15 +6,15 @@ f = 8000; % Frequência entrada em Hz
 %% Etapa 1 => Filtro de Entrada
 filtro = designfilt('lowpassiir', 'PassbandFrequency', 1000, 'StopbandFrequency', 1500, 'PassbandRipple', 1, 'StopbandAttenuation', 60, 'SampleRate', f);
 sinal_filtrado = filter(filtro, xt);
-
+%fvtool(filtro);
 %% Etapa 2 => Amostragem
 fs = 4000; % Frequência de amostragem em Hz
 ffc = f/fs; % Pega o quanto fs é menor que f
 
 Ts = 1/fs;
 N = length(t)/ffc; % A quantidade de amostras é 1/fcc menor do que o tamanho de amostras iniciais
-n = 0 : 1 : N-1;
-t_amostrado = 0 : Ts : n(N)*Ts;
+n = [0 : 1 : N-1];
+t_amostrado = [0 : Ts : n(N)*Ts];
 sinal_amostrado = zeros(1);
 k = 0;
 
@@ -24,34 +24,17 @@ for i = 1 : 1 : N
 end
 
 %% Faz o plot dos resultados
+% Figura 1: x(t) sem filtro no domínio do tempo e seu espectro em
+% frequência
+figure(1);
+subplot(2,1,1);
+plot(t, xt);
+xlabel('$t$(s)','Interpreter','LaTex')
+ylabel('Amplitude')
+title('$x(t) = x_{f}(t) + x_{r}(t)$ ','Interpreter','LaTex','FontSize',14);
+axis([0 0.05 -inf inf]);
 
-% Figura 1: x(t) filtrado
-figure(1)
-subplot(3,1,1)
-plot(t, sinal_filtrado);
-xlabel('$t$','Interpreter','LaTex','FontSize',18)
-ylabel('$x(t)$','Interpreter','LaTex','FontSize',18)
-title('x(t) filtrado');
-
-% Figura 1: x[t] amostrado
-subplot(3,1,2)
-stem(n, sinal_amostrado); 
-xlabel('$n$','Interpreter','LaTex','FontSize',18)
-ylabel('$x[n]$','Interpreter','LaTex','FontSize',18)
-title('x[t] amostrado');
-
-% Figura 1: x(t) e x[t]
-subplot(3,1,3)
-plot(t, sinal_filtrado);
-hold;
-plot(t_amostrado, sinal_amostrado, 'o');
-xlabel('$t$','Interpreter','LaTex','FontSize',18)
-ylabel('$x[nT_s],x(t)$','Interpreter','LaTex','FontSize',18)
-title('x(t) e x[n]');
-
-% Figura 2: Espectro de x(t) sem o filtro
-figure(2)
-subplot(3,1,1);
+subplot(2,1,2);
 y=fft(xt); grid on;
 yaux=fliplr(y(1,2:end));
 X=[yaux y];
@@ -65,10 +48,19 @@ length(w);
 plot(w,abs(2*X/length(t)));
 xlabel('$f$(Hz)','interpreter','latex');
 ylabel('Magnitude');
+title('$|X(j\omega)|$ ','Interpreter','LaTex','FontSize',14);
+axis([-8000 8000 -inf inf]);
 
-% Figura 2: Espectro de x(t) com o filtro
-figure(2)
-subplot(3,1,2);
+% Figura 2: x(t) filtrado e seu espectro em frequência
+figure(2);
+subplot(2,1,1);
+plot(t, sinal_filtrado);
+xlabel('$t$(s)','Interpreter','LaTex')
+ylabel('Amplitude')
+title('$x_{cc}(t)$','Interpreter','LaTex','FontSize',14);
+axis([0 0.05 -inf inf]);
+
+subplot(2,1,2);
 y=fft(sinal_filtrado); grid on;
 yaux=fliplr(y(1,2:end));
 X=[yaux y];
@@ -82,10 +74,20 @@ length(w);
 plot(w,abs(2*X/length(t)));
 xlabel('$f$(Hz)','interpreter','latex');
 ylabel('Magnitude');
+title('$|X_{cc}(j\omega)|$','Interpreter','LaTex','FontSize',14);
+axis([-8000 8000 -inf inf]);
 
-% Figura 2: Espectro de x[n]
-figure(2)
-subplot(3,1,3);
+% Plot sinal amostrado e discretizado
+figure(3);
+subplot(2,1,1);
+stem(n, sinal_amostrado); 
+xlabel('$n$','Interpreter','LaTex')
+ylabel('Amplitude')
+title('$x_{cc}(t) = x_{cc}(nT) = x[n]$ amostrado e discretizado', 'Interpreter','LaTex','FontSize',14);
+axis([0 50 -inf inf]);
+
+
+subplot(2,1,2);
 y=fft(sinal_amostrado); grid on;
 yaux=fliplr(y(1,2:end));
 X=[yaux y];
@@ -99,44 +101,106 @@ length(w);
 plot(w,abs(2*X/length(t)));
 xlabel('$f$(Hz)','interpreter','latex');
 ylabel('Magnitude');
+title('$|X(e^{j\omega})|$','Interpreter','LaTex','FontSize',14);
+axis([-8000 8000 -inf inf])
+
+% Plot sinal filtrado e sinal amostrado
+figure(4);
+plot(t, sinal_filtrado);
+hold;
+plot(t_amostrado, sinal_amostrado, 'o');
+xlabel('$t$(s)','Interpreter','LaTex')
+ylabel('Amplitude')
+title('$x_{cc}(t)$ e $x_{amostrado}(t)$','Interpreter','LaTex','FontSize',14);
+axis([0 0.05 -inf inf]);
+legend('x_{cc}','x_{amostrado}');
 
 %% Mudança da taxa de amostragem
 
-M = 2
+M = 2;
 sinal_subamostrado = downsample(sinal_amostrado,M);
 t_subamostrado = downsample(t_amostrado,M);
 Td = M*Ts;
 fd = 1/Td;
 Nd = N/M;
 nd = 0 : 1 : Nd-1;
-figure(3)
+figure(5)
 plot(t_amostrado, sinal_amostrado, t_subamostrado, sinal_subamostrado);
-xlabel('$t_d$','Interpreter','LaTex','FontSize',18);
-ylabel('$y(nT), y(nT_d)$','Interpreter','LaTex','FontSize',18);
-
+xlabel('$t$(s)','Interpreter','LaTex')
+ylabel('Amplitude')
+title('$y(nT)$, $y(nT_d)$','Interpreter','LaTex','FontSize',14);
+axis([0 0.05 -inf inf]);
+legend('y(nT)','y(nT_d)');
 
 %% Reconstrução
 
 sinal_reconstruido = sinal_subamostrado*sinc(fd*(ones(length(nd),1)*t-(nd*Td)'*ones(1, length(t))));
-
-figure(4)
-subplot(3,1,1);
-plot(t, sinal_filtrado);
-xlabel('$t$','Interpreter','LaTex','FontSize',18);
-ylabel('$x_c(t)$','Interpreter','LaTex','FontSize',18);
-
-subplot(3,1,2);
+% Plot sinal reconstruído
+figure(6);
+subplot(2,1,1);
 plot(t, sinal_reconstruido);
-xlabel('$t$','Interpreter','LaTex','FontSize',18);
-ylabel('$x_r(t)$','Interpreter','LaTex','FontSize',18);
+xlabel('$t$(s)','Interpreter','LaTex')
+ylabel('Amplitude')
+title('$y_{cc}(t)$','Interpreter','LaTex','FontSize',14);
+axis([0 0.05 -inf inf]);
 
-subplot(3,1,3);
+subplot(2,1,2);
+y=fft(sinal_reconstruido); grid on;
+yaux=fliplr(y(1,2:end));
+X=[yaux y];
+X(1,1:length(X)/4)=0;
+X(1,3*length(X)/4:end)=0;
+length(X);
+omega=0:fs/length(y):fs-(fs/length(y));
+waux=-fliplr(omega(1,2:end));
+w=[waux omega];
+length(w);
+plot(w,abs(2*X/length(t)));
+xlabel('$f$(Hz)','interpreter','latex');
+ylabel('Magnitude');
+title('$|Y_{cc}(j\omega)|$','Interpreter','LaTex','FontSize',14);
+axis([-8000 8000 -inf inf])
+
+% Plot sinal reconstruído e filtrado juntos
+figure(7);
 plot(t, sinal_filtrado);
 hold;
 plot(t, sinal_reconstruido);
-xlabel('$t$','Interpreter','LaTex','FontSize',18)
-ylabel('$x_r(t),x_c(t)$','Interpreter','LaTex','FontSize',18)
+xlabel('$t$(s)','Interpreter','LaTex')
+ylabel('Amplitude')
+title('$y_{cc}(t)$ e $x_{cc}(t)$ ','Interpreter','LaTex','FontSize',14);
+axis([0 0.05 -inf inf]);
+legend('x_{cc}','y_{cc}');
 
+figure(8);
+subplot(2,1,1);
+plot(t, xt);
+hold;
 plot(t, sinal_reconstruido);
+xlabel('$t$(s)','Interpreter','LaTex')
+ylabel('Amplitude')
+title('$y_{cc}(t)$ e $x(t)$ ','Interpreter','LaTex','FontSize',14);
+axis([0 0.05 -inf inf]);
+legend('x','y_{cc}');
 
-sound(sinal_reconstruido, f);
+subplot(2,1,2);
+plot(w,abs(2*X/length(t)));
+hold;
+y=fft(xt); grid on;
+yaux=fliplr(y(1,2:end));
+X=[yaux y];
+X(1,1:length(X)/4)=0;
+X(1,3*length(X)/4:end)=0;
+length(X);
+omega=0:f/length(y):f-(f/length(y));
+waux=-fliplr(omega(1,2:end));
+w=[waux omega];
+length(w);
+plot(w,abs(2*X/length(t)));
+xlabel('$f$(Hz)','interpreter','latex');
+ylabel('Magnitude');
+title('$|Y_{cc}(j\omega)|$ e $|X(j\omega)|$','Interpreter','LaTex','FontSize',14);
+axis([-8000 8000 -inf inf])
+
+%sound(sinal_reconstruido, f);
+
